afffe94502f0a37cda5a0820f70683f695082d8f
AboAuther <1256446353@qq.com>
2021-05-12 17:01:03 +0800

    pointers->bitcoin_wallet

---
+++ pointers/v4/v4_wallet.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Wallet struct {
+	balance Bitcoin
+}
+type Bitcoin int
+
+var ErrInsufficientFunds = errors.New("cannot withdrew,insufficient funds")
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
