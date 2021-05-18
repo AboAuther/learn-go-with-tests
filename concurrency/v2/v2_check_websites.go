8eada34f5da035c19ff24032c88985148e36bcd0
AboAuther <1256446353@qq.com>
2021-05-18 19:03:01 +0800

    concurrency map

--- concurrency/v2/v2_check_websites.go
+++ concurrency/v2/v2_check_websites.go
@@ -1,16 +1,25 @@
 package concurrency
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type WebsiteChecker func(string) bool
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
-	results := make(map[string]bool)
+	var c = struct {
+		sync.RWMutex
+		result map[string]bool
+	}{result: make(map[string]bool)}
+	//results := make(map[string]bool)
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			c.Lock()
+			c.result[u] = wc(u)
+			c.Unlock()
 		}(url)
 	}
 	time.Sleep(2 * time.Second)
-	return results
+	return c.result
 }
