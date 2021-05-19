368c85b85ede80465e5fbb9d6171aa8a33c07461
AboAuther <1256446353@qq.com>
2021-05-19 22:31:07 +0800

    concurrency

--- concurrency/v2/v2_check_websites.go
+++ concurrency/v2/v2_check_websites.go
@@ -8,18 +8,19 @@ import (
 type WebsiteChecker func(string) bool
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
-	var c = struct {
-		sync.RWMutex
-		result map[string]bool
-	}{result: make(map[string]bool)}
-	//results := make(map[string]bool)
+	//var c = struct {
+	//	sync.RWMutex
+	//	result map[string]bool
+	//}{result: make(map[string]bool)}
+	result := make(map[string]bool)
+	Mutex := sync.RWMutex{}
 	for _, url := range urls {
 		go func(u string) {
-			c.Lock()
-			c.result[u] = wc(u)
-			c.Unlock()
+			Mutex.Lock()
+			result[u] = wc(u)
+			Mutex.Unlock()
 		}(url)
 	}
 	time.Sleep(2 * time.Second)
-	return c.result
+	return result
 }
