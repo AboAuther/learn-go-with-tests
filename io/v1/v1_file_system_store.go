8cc5a88490764781c27b56b2e36235b20fcab4f5
AboAuther <1256446353@qq.com>
2021-05-22 22:38:40 +0800

    io test

---
+++ io/v1/v1_file_system_store.go
@@ -0,0 +1,13 @@
+package main
+
+import "io"
+
+type FileSystemPlayerStore struct {
+	database io.ReadSeeker
+}
+
+func (f *FileSystemPlayerStore) GetLeague() []Player {
+	f.database.Seek(0, 0)
+	league, _ := NewLeague(f.database)
+	return league
+}
